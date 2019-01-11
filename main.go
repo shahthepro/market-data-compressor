f2ccc0b5d227a92c571b158316c2357033fae51d
Shahul Hameed <git@github.com>
2019-01-11 20:46:11 +0530

    Add main.go and Add main() to read file and print its contents

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Panic("Cannot read file:", err.Error())
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
